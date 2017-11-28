769a36eff548f71ea4198767539705d5e9103504
jd.meek <jd.meek@premisehealth.com>
2017-11-28 00:43:32 -0600

    Added to the base README file, created /Cygwin and added Get-FastestCygwin.ps1 (v1.0).

---
+++ Cygwin/Get-FastestCygwin.ps1
@@ -0,0 +1,177 @@
+﻿#requires -version 2
+<#
+.SYNOPSIS
+  This script will determine the fastest Cygwin Mirror for your location.
+
+.DESCRIPTION
+  This script will determine the fastest Cygwin Mirror for your location.
+
+.PARAMETER <Parameter_Name>
+    <Brief description of parameter input required. Repeat this attribute if required>
+
+.INPUTS
+  NONE
+
+.OUTPUTS
+  Displays the 10 fastest cygwin hosts
+
+.NOTES
+  Version:        1.0
+  Author:         J.D. Meek
+  Creation Date:  01-03-2017
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  Get-FastestCygwin
+#>
+
+
+#---------------------------------------------------------[Initialisations]-------------------------------------------------------
+#Script Version
+$sScriptVersion = "1.0"
+
+#Set Error Action to Silently Continue
+$ErrorActionPreference = "SilentlyContinue"
+$progressPreference = 'silentlyContinue'
+
+
+<#--
+    Without switches, this script will determine the fastest Cygwin Mirror for your location.
+    If you add the -install switch, it will download cygwin-setup-x64 and will start a manual installation.
+    If you add the -install -silent switch, it will look for the cygwin-install.def file.  If not found, a
+    basic install will be performed.
+
+    http://cygwin.com/mirrors.html
+     system("cat mirrors.html | grep -A500 'Site list by region' | grep -B500 'Mirror Administrators' | sed -e s/\\ /\\\\n/g | sed -e s/\\,/\\\\n/g  > tmp.1");
+  system("cat tmp.1 | sed -e s/\\</\\\\n/g | grep href | grep -v 'rsync' | cut -f 2 -d \\\" | sort | uniq > cygwin_mirror.list ");
+
+--#>
+
+$Debug = 1
+
+$Results = @()
+$StartDir = (Get-Item -Path ".\" -Verbose).FullName
+$WorkDir = "$Env:AppData\CygDiscover"
+
+if ($env:PROCESSOR_ARCHITECTURE -eq "AMD64")
+    {
+    $Arch = "x86_64"
+    }
+    else
+    {
+    $Arch = "x86"
+    }
+
+if (Test-Path $WorkDir) {
+    try {
+        Remove-Item -Recurse -Force $WorkDir
+        }
+        catch
+        {
+        Write-Error "Unable to clear the existing work directory.`n Please ensure that another copy of this script is not running."
+        Exit 1
+        }
+
+}
+
+New-Item $WorkDir -ItemType Directory
+Set-Location $WorkDir
+
+$Mirrors = Invoke-WebRequest http://cygwin.com/mirrors.html 
+$URLS = $Mirrors.Links | where {($_.innerHTML -Match 'com$|net$|org$') -AND ($_.href -NotMatch 'rsync:')} | Select outerText,href
+
+foreach ($BASE in $URLS)
+     {
+
+     Remove-Variable $Time
+
+     $URI = $BASE.href + $Arch + "/setup.xz"
+
+     $Site = $BASE.href.split("/",4)
+     $HostName = $Site[2]
+
+     Write-Host -NoNewline "Checking $HostName... " 
+     
+     try 
+        { $Time = Measure-Command {$URITime = Invoke-WebRequest $URI} }
+        catch
+        {
+        if ($Debug) {
+            Write-Host -ForegroundColor Red "Failed!"
+            }
+
+        Continue
+        }
+
+     Write-Host $Time
+
+     $TmpObj = New-Object PSObject 
+     $TmpObj | Add-Member -type NoteProperty -Name HostName -Value $HostName
+     $TmpObj | Add-Member -type NoteProperty -Name URL -Value $URI.Replace("setup.xz","")
+     $TmpObj | Add-Member -type NoteProperty -Name Ticks -Value $Time.Ticks
+
+     $Results += $TmpObj
+
+     }
+
+$TopTen = $Results | Sort Ticks | Select -First 10
+$Fastest = $TopTen[0].HostName
+
+Write-Host "Fastest Host: $Fastest"
+
+Write-Host "Would you like to fetch the latest installer and start using this host?"
+while("yes","no","y","n" -notcontains $answer)
+{
+	$answer = Read-Host "Yes or No"
+}
+
+if ($answer -eq "YES" -or $Answer -eq "Y") {
+<#--
+Setup Command Line Options:
+ -A --disable-buggy-antivirus           Disable known or suspected buggy anti
+                    virus software packages during execution.
+ -C --categories                        Specify entire categories to install
+ -D --download                          Download from internet
+ -d --no-desktop                        Disable creation of desktop shortcut
+ -h --help                              print help
+ -K --pubkey                            Path to extra public key file (gpg format)
+ -L --local-install                     Install from local directory
+ -l --local-package-dir                 Local package directory
+ -n --no-shortcuts                      Disable creation of desktop and start menu
+                    shortcuts
+ -N --no-startmenu                      Disable creation of start menu shortcut
+ -O --only-site                         Ignore all sites except for -s
+ -P --packages                          Specify packages to install
+ -p --proxy                             HTTP/FTP proxy (host:port)
+ -q --quiet-mode                        Unattended setup mode
+ -r --no-replaceonreboot                Disable replacing in-use files on next
+                    reboot.
+ -R --root                              Root installation directory
+ -S --sexpr-pubkey                      Extra public key in s-expr format
+ -s --site                              Download site
+ -U --keep-untrusted-keys               Use untrusted keys and retain all
+ -u --untrusted-keys                    Use untrusted keys from last-extrakeys
+ -X --no-verify                         Don't verify setup.ini signatures
+--#>
+
+    $CygRoot = $(Get-ItemProperty HKLM:\SOFTWARE\Cygwin\setup).rootdir
+    $SetupFile="setup-$Arch.exe"
+    $WorkSetup = "$Workdir\$SetupFile"
+    Write-Host "Fetching Setup File."
+    Invoke-WebRequest "http://cygwin.org/$SetupFile" -OutFile $WorkSetup
+    Write-Host "Done...  Executing."
+
+    $CygArgs = "--root $CygRoot --quiet-mode --upgrade-also --site $Fastest"
+
+    Invoke-Expression "$WorkSetup $CygArgs"
+    }
+
+if ($ShowTopTen) {
+    Write-Host "Top Ten`n`n$TopTen"
+    }
+
+Set-Location $StartDir
+
+$progressPreference = 'Continue' 
+
+Exit
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Administrivia
 Various admin tools and goodies written in powershell...bash...python...perl...batch...
+
+Basically, this is my collection of tools that I've written over the years.  Some are good, 
+some are indifferent and a couple are bad hacks but all did the job and seemed worthy of keeping.
+I tend to write these tools with the primary UNIX directive in mind.  I'm almost sure that you're 
+wondering what this "primary Unix directive" is, unless you already know.  But, if not...
+
+WRITE A TOOL THAT DOES ONLY ONE THING BUT DOES THAT THING REALLY, REALLY WELL.
+You know, like grep/awk/sed.
+
+The tools are sorted into folders by the primary target that they served.  Windows, Linux, Cygwin
+Splunk, etc... Inside each folder you will find a collection of scripts that serve that target in 
+some way.  It could be a fix, settings application, tuning, reporting, or some other equally tedious 
+task related to that target.  When you find a subfolder, that will be a collection relating to one 
+thing.  A task handler, admin toolset or other app or library type thing,
+
+The only path to true enlightenment is to read the README files.  That should have everything you 
+might want to know about what you're looking at.  Well, they at least have everything that I felt
+like writing about before wondering off in a squirrel moment.  Also, read the code.  I try to comment
+liberally so there's goodness in them thar files.
+
+You will likely note the use of a variety of languages in any given folder.  That's because I either
+determined that the selected language was the right one or, quite possibly, I used the project as a 
+learning exercise for that language.  
+
+Finally, I'm sure that I'll break this up into several repos as I dig through the results of 25 years 
+running DOS/WIN, Linux, AIX, Solaris MacOS, OSX, HPUX, DGUX and the various app/db/thingy-madoodle 
+servers that run on them.  I have other repos available at https://github.com/jdmeek.  They are also 
+specific to the application in the folder name but have gotten either large enough or focused enough
+to warrant their own place in the world.
+
+Finally, any file that has -tuning in the name is going to show my recommended tuning params/methods
+for that platform/product.
+
+Any way, I hope you find them useful or at least amusing.  I know that some of it is pretty bad code or
+would have been cleaner another way but, hey, they all worked for the purpose.  That being said, unless
+you're contributing something constructive, I really don't want to hear about how big of a dumbass I am
+because I didn't use XYZ language of the month or some lame-assed half-baked framework that some zitfaced
+script-kiddie thought up for god-only-knows what purpose related to which-ever MMORPG happens to be the
+flavor of the day.  So, either contribute or STFU.  No one has time for trolls and you will likely be
+publicly filleted if it becomes tiresome.
+
+
+
